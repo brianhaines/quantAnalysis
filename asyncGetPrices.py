2ef38840a21c06bb14b79dfe66583f4ccfd8be73
DevOnLinux <b.ge.haines@gmail.com>
2014-09-08 18:32:53 -0400

    Further refinements and experimentation with pandas.

--- asyncGetPrices.py
+++ asyncGetPrices.py
@@ -15,18 +15,18 @@ def fetch_page(url, idx):
     global resList
     r = []
     response = yield from aiohttp.request('GET', url)
-    r = yield from response.read_and_close()
+    # r = yield from response.read_and_close()
     r = yield from response.json()
+    
     resList.append(r)
     return (r)
 
 def main():  
     url = 'http://yahooserver.herokuapp.com/prices/'
     tickers = ['XOM','CVX','BP','SPY','XLE']
-    nPrices = 2000
+    nPrices = 3000
 
     urls=[]
-
     for each in tickers:
     	s = url+each+'&'+str(nPrices)
     	urls.append(s)
@@ -34,7 +34,6 @@ def main():
     coros = []
     for idx, url in enumerate(urls):
         coros.append(asyncio.Task(fetch_page(url, idx)))
-
     yield from asyncio.gather(*coros)
 
 
@@ -44,14 +43,23 @@ if __name__ == '__main__':
     loop.run_until_complete(main())
     print('Response took: ', datetime.now()-t, 'seconds.')
 
-    for each in resList:
-        print(each['Ticker'])
+    # for each in resList:
+    #     print(each['Ticker'])
 
+    #Convert all those numbers to a clean pandas dataFrame
     outFrame = ch.cleaner(resList)
     
-    # print(len(resList))
-    # for each in resList:
-    # 	print(each)
-    print(outFrame)
-    print(len(outFrame))
+    # Regression
+    # from pandas.stats.api import ols
+    # res = ols(y=outFrame['XOM'],x=outFrame['CVX'])
+    # print(res)
+    # Run some regressions
+    print(outFrame.cov())
+    # print(outFrame)
+    # rollMean = pd.rolling_mean(outFrame['XOM'],60)
+    # pd.rolling_corr(outFrame.XOM,outFrame.CVX,50).plot(style='k')
 
+    correls = pd.rolling_corr(outFrame.XOM,outFrame.CVX, 50,pairwise=True)
+    print(correls)
+    # print(outFrame.XOM)
+    # print(outFrame.DateTime.min(), rollCorr.min())
