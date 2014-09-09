56a578456a5288b2833c1d9062f2fdfcb022103c
DevOnLinux <b.ge.haines@gmail.com>
2014-09-09 13:28:23 -0400

    Fixed a bug in the merge/sort function. Sort was not handling datetime objects correctly, so sorting has been moved to a standalone call on the dataframe.

--- cleanHistory.py
+++ cleanHistory.py
@@ -5,6 +5,8 @@ that are common between the series being analysed.'''
 from datetime import datetime
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 def cleaner(pList):
     if len(pList)<2:
@@ -18,18 +20,18 @@ def cleaner(pList):
             d1=[]
             v1=[]
             for i in series['Prices']:
-                d1.append(''.join([i[0],'T',i[1]]))
+                d1.append(''.join([i[0],' ',i[1]]))
                 v1.append(i[2])
             a1 = np.array([d1,v1])
             dfOut=pd.DataFrame(a1).T
             labels.append(series['Ticker'])
-            dfOut.columns = ['DateTime',series['Ticker']]            
+            dfOut.columns = ['DateTime',series['Ticker']]
         elif sid > 0:
             #Make a new dataframe for each subsequent series
             dN=[]
             vN=[]
             for i in series['Prices']:
-                dN.append(''.join([i[0],'T',i[1]]))
+                dN.append(''.join([i[0],' ',i[1]]))
                 vN.append(i[2])
             aN = np.array([dN,vN])
             dfN=pd.DataFrame(aN).T
@@ -37,12 +39,15 @@ def cleaner(pList):
             dfN.columns = ['DateTime',series['Ticker']]
             #Now we have the initial and Nth series in dataframe format
 
-            # This will merge the Nth df with the initial df
-            dfOut = pd.DataFrame(np.array(dfOut.merge(dfN, on='DateTime', how='outer').sort('DateTime'),),columns=labels)
+            # This will merge the Nth df with the initial df.
+            dfOut = pd.DataFrame(np.array(dfOut.merge(dfN, on='DateTime', how='outer'),),columns=labels)
 
     # All of the stocks have been added to the dataframe, now clean it up
     dfClean = dfOut.dropna()
-    
+    # dfClean.index = dfClean['DateTime']
+    dfClean['DateTime']=pd.to_datetime(dfClean['DateTime'])
+    dfClean = dfClean.sort(columns='DateTime',ascending=True)
+
     labels.pop(0)
     dfClean[labels] = dfClean[labels].astype(float) #This raises a soft warning.
     #Trying to change price values dtype to float. The line above works but raises an error:
@@ -54,7 +59,5 @@ def cleaner(pList):
     # dfClean.iloc[dfClean[:,:]].astype(float)
     # dfClean.loc[dfClean[:,labels]].astype(float)
     # print(dfClean.dtypes)
-        
     # dfClean['diff'] = dfClean[pList[1]['Ticker']].sub(dfClean[pList[0]['Ticker']], axis=0)    
-
     return(dfClean)
